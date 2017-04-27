9c0d72809c7bf3b7dd9f928bda58f6e92af2ab55
Egor BEREZOVSKIY <ebe@zengularity.com>
2017-04-27 11:29:53 +0200

    Bootstrap

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import * as express from "express"
+import * as GraphHttp from "express-graphql"
+//import schema from "./schemas/schema"
+
+const APP_PORT = 3000
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.end("Hello World")
+})
+
+/*
+app.use("/graphql", GraphHttp(request => ({
+  schema,
+  pretty: true,
+  graphiql: true,
+  rootValue: {
+    request: request,
+  }
+})))
+*/
+
+app.listen(APP_PORT, () => {
+  console.log(`App listening on port ${APP_PORT}`)
+})
