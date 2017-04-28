b180a8317d39db27121b7c6e7bebdc0d939fad2c
Nicolas Glondu <ngl@zengularity.com>
2017-04-28 15:17:17 +0200

    poster service

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 var path = require('path');
+var fs = require('fs');
 import * as express from "express"
 import * as GraphHttp from "express-graphql"
 import "./models/db"
 import schema from "./schemas/schema"
+import * as mongoose from "mongoose"
+
 const APP_PORT = 3000
+
+const Movie = mongoose.model("movies");
+const http = require('http');
 
 const app = express()
 
-app.use(express.static(path.join(__dirname + '/../public')))
+app.use(express.static(__dirname + '/../public'))
 
 app.use("/graphql", GraphHttp(request => ({
   schema,
@@ -17,6 +23,27 @@ app.use("/graphql", GraphHttp(request => ({
     request: request,
   }
 })))
+
+app.get("/img/:id", (req, res) => {
+  const { id } = req.params;
+  const localPath = path.join(__dirname + '/../public/posters/' + id);
+  console.log(localPath)
+  if (fs.existsSync(localPath)) {
+    res.contentType("image/jpg").sendFile(localPath);
+  } else {
+    Movie.findOne({_id: id}).exec(
+      (err, doc: any) => {
+        if (doc) {
+          const url = `http://fr.web.img3.acsta.net${doc.poster}`;
+          console.log("Caching "+url);
+          const file = fs.createWriteStream(localPath);
+          http.get(url, (response: any) => response.pipe(file))
+          setTimeout(() => res.contentType("image/jpg").sendFile(localPath), 300);
+        } else res.end("Incorrect movie")
+      }
+    )
+  }
+})
 
 app.listen(APP_PORT, () => {
   console.log(`App listening on port ${APP_PORT}`)
